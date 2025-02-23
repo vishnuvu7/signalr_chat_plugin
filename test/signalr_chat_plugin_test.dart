a91afed28c6193dada0d220d884f4a9e73148ce2
go2hyder <go2hyder@gmail.com>
2025-02-23 17:12:37 +0400

    added web

---
+++ test/signalr_chat_plugin_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:signalr_chat_plugin/signalr_chat_plugin.dart';
+import 'package:signalr_chat_plugin/signalr_chat_plugin_platform_interface.dart';
+import 'package:signalr_chat_plugin/signalr_chat_plugin_method_channel.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class MockSignalrChatPluginPlatform
+    with MockPlatformInterfaceMixin
+    implements SignalrChatPluginPlatform {
+
+  @override
+  Future<String?> getPlatformVersion() => Future.value('42');
+}
+
+void main() {
+  final SignalrChatPluginPlatform initialPlatform = SignalrChatPluginPlatform.instance;
+
+  test('$MethodChannelSignalrChatPlugin is the default instance', () {
+    expect(initialPlatform, isInstanceOf<MethodChannelSignalrChatPlugin>());
+  });
+
+  test('getPlatformVersion', () async {
+    SignalrChatPlugin signalrChatPlugin = SignalrChatPlugin();
+    MockSignalrChatPluginPlatform fakePlatform = MockSignalrChatPluginPlatform();
+    SignalrChatPluginPlatform.instance = fakePlatform;
+
+    expect(await signalrChatPlugin.getPlatformVersion(), '42');
+  });
+}
